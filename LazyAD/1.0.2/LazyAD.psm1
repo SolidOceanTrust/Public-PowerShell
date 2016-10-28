93ca815b6effe4f35f77aaa8e1a7e13e99518f72
Solid Ocean Trust <BNabel$@tcallc.local>
2016-10-28 17:16:31 -0400

    Version 1.0.2! Adds -filter switch to Get-ADGroupMemberLike and Get-ADUserLike cmdlets. Removes default filtering options in all pervious versions. Fixes readme file with odd inputs. TODO is in the readme file!

---
+++ LazyAD/1.0.2/LazyAD.psm1
@@ -0,0 +1,81 @@
+﻿Function Get-ADGroupLike
+{
+    [CmdletBinding()]
+    [Alias("adglike")]
+    param(
+            [string]$Like
+        )#param
+
+    $InputFinal = "*$Like*"
+    Get-ADGroup -Filter 'Name -like $InputFinal'
+}
+
+Function Get-ADGroupMemberLike
+{
+    [CmdletBinding()]
+    [Alias("adgmlike")]
+    param(
+            [string]$group,
+            [switch]$Recursive = $false,
+            [switch]$Filter = $false
+        )#param
+    
+    $basecmd = $("Get-ADGroupMember -Identity `"$group`"")
+    
+    Switch ($Recursive)
+    {
+        $true { $basecmd += " -Recursive" }
+        $false {}
+    }#Switch ($Recursive)
+    
+
+    Switch ($Filter)
+    {
+        $true {$basecmd += " | Select Name,SamAccountName,distinguishedName"}
+        $false {}
+    }#Switch ($Recursive)
+
+    #Final Output
+    $finalcmd = $basecmd.ToString()
+
+    Invoke-Expression $finalcmd
+}
+
+Function Get-ADUserLike
+{
+    [CmdletBinding()]
+    [Alias("adulike")]
+    param(
+            [string]$Like,
+            [switch]$Filter = $false
+        )#param
+
+    $InputFinal = "*$Like*"
+    $basecmd = $("Get-ADUser -Filter 'Name -like `"$InputFinal`"' -Properties *")
+
+     Switch ($Filter)
+    {
+        $true {$basecmd += " | Select Name,SamAccountName,UserPrincipalName,LockedOut,Enabled"}
+        $false {}
+    }#Switch ($Recursive)
+
+    $finalcmd = $basecmd.ToString()
+
+    Invoke-Expression $finalcmd
+
+    #Get-ADUser -Filter 'Name -like $InputFinal' -Properties * | Select Name,SamAccountName,UserPrincipalName,LockedOut,Enabled
+}
+
+Function Get-ADComputerLike
+{
+    [CmdletBinding()]
+    [Alias("adclike")]
+    param(
+            [string]$Like
+        )#param
+
+    $InputFinal = "*$Like*"
+    Get-ADComputer -Filter 'Name -like $InputFinal' -Properties * | Select Name,DNSHostName,Enabled,SamAccountName,SID,ObjectClass
+}
+
+Export-ModuleMember -function * -alias *
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-** Update: 10/28/16. Added LazyAD Module version 1.0.10/28/16
+** Update: 10/28/16. Added LazyAD Module version 1.0.2
+	* Fixes README typo.
+	* Added ReleaseNotes section inside of LazyAD.psd1 file (helpful for using "Publish-Module" to an internal repo)
+	* Removed default filtering on user group and ad group members. Added -Filter switch parameter to Get-ADGroupMemberLike and Get-ADUserLike
+	* TODO: Add comments, examples and -Filter switch to Get-ADComputerLike
+
+
+** Update: 10/28/16. Added LazyAD Module version 1.0.1
 	* Adds function Get-ADComputerLike to the module.
 	* Still need to add comments...
 
