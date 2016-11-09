0719146e6c0a918bdb86d56cf295bd132016d020
SolidOceanTrust <ben.nabel@outlook.com>
2016-11-09 16:56:20 -0500

    removes old scripts. adds new script for bitlocker status

--- Scripts/New-ModuleVersion.ps1
+++ Scripts/New-ModuleVersion.ps1
@@ -3,22 +3,28 @@ $BaseBuildDir = 'D:\Git\Public-PowerShell'
 $FinalBuildDir = (Join-Path $BaseBuildDir $ModuleName)
 $ModuleDir = "$env:ProgramFiles\WindowsPowerShell\Modules"
 $LatestBuild = (((Get-Module -ListAvailable | Where{$_.Name -eq $ModuleName} | Select -first 1) | Select -expand Version))
-[version]$NewBuild = '1.0.3'
+[version]$NewBuild = '1.0.4'
 
 if(($LatestBuild -eq $NewBuild))
 {
     Throw "Versions are Equal"
 }
 
-
 # Copy the Modules to $BaseBuildDir (for Git)
 Copy-Item "$ModuleDir\$moduleName\$LatestBuild" "$FinalBuildDir\$NewBuild" -Recurse -Force
 
 Set-Location "$FinalBuildDir\$NewBuild"
-#Get-ChildItem -Path $BaseBuildDir -Directory | Where{$_.LastWriteTime} 
 
 $OriginalConfig = Get-Content $($ModuleName+".psd1")
-$moduleInfo = (Import-LocalizedData -file $($ModuleName+".psd1")).ModuleVersion
+
+$moduleRAW = (Import-LocalizedData -BaseDirectory . -FileName $($ModuleName+".psd1"))
+$moduleVersion = $moduleRAW.ModuleVersion
+
+if($moduleVersion -eq $LatestBuild)
+{ Throw "Module from $modulesDIR doesn't eq Module version from file" }
+
+#$moduleInfo  = (Import-LocalizedData -BaseDirectory . -FileName $($ModuleName+".psd1")).ModuleVersion
+
 $CurrentVersion = $LatestBuild.ToString()
 $NewVersion = $NewBuild.ToString()
 
@@ -26,6 +32,4 @@ $OriginalConfig | ForEach-Object { $_ -replace $CurrentVersion,$NewVersion} | Se
 
 Rename-Item $($ModuleName+".psd1") $($ModuleName+".psd1.old")
 Rename-Item TempData.psd1 $($ModuleName+".psd1")
-
-#Set-Location "D:\Git\Public-PowerShell\LazyAD\1.0.3"
-#Import-LocalizedData -FileName Temp1.psd1
+Remove-Item $($ModuleName+".psd1.old") -Confirm:$false
