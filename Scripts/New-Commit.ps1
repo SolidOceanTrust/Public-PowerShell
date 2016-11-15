b99b0224ea632ac725da4eb1d424f281466ce265
SolidOceanTrust <ben.nabel@outlook.com>
2016-11-15 17:16:35 -0500

    adds gitignore and updates LastPass_API logic

---
+++ Scripts/New-Commit.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.Synopsis
+   Gets BitLocker Recovery Key
+.DESCRIPTION
+   Grabs the BitLocker Recovery key from AD. Accepts OU or computer name
+.EXAMPLE
+   Get-BLKey -computer mypc
+.EXAMPLE
+   Get-BLKey -ADSearchBase "OU=Computers,OU=Site,DC=Domain,DC=corp"
+#>
+Function New-Commit
+{
+    [CmdletBinding()]
+    [Alias("nc","gitc")]
+    [OutputType([string])]
+
+    param (
+            [Alias("msg","cm")] 
+            [string]$CommitMsg
+        )
+    
+    try {
+            git add --all
+            git commit -m $CommitMsg
+            git push
+
+        }
+    catch {
+            Write-Error -Exception $_
+        }
+
+}
